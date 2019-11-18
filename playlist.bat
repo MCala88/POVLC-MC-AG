479c1bd1f23e608709df92a76ed68a6536ba777b
Miquel Calafell <56043308+MCala88@users.noreply.github.com>
2019-11-18 21:43:02 +0100

    Create playlist.bat

---
+++ playlist.bat
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+
+mkdir /home/$USER/VLC_playlist
+cd /home/$USER/VLC_playlist
+
+mkdir Daft_Punk
+mv /home/$USER/Descargar/Get_Lucky-Daft_Punk.mp3 /home/$USER/VLC_playlist/Daft_Punk/
+mv /home/$USER/Descargar/One_more_time-Daft_Punk.mp3 /home/$USER/VLC_playlist/Daft_Punk/
+mv /home/$USER/Descargas/Around_the_world-Daft_Punk.mp3 /home/$USER/VLC_playlist/Daft_Punk/
+mv /home/$USER/Descargas/Instant_crush-Daft_Punk.mp3 /home/$USER/VLC_playlist/Daft_Punk/
+
+mkdir Fuego
+mv /home/$USER/Descargas/Una Vaina Loca-Fuego.mp4 /home/$USER/VLC_playlist/Fuego/
+
+mkdir Josue_Yrion
+mv /home/$USER/Descargas/Los_nintendo-Josue_Yrion.mp3 /home/$USER/VLC_playlist/Josue_Yrion/
+mv /home/$USER/Descargas/Los_pokimons-Josue_Yrion.mp3 /home/$USER/VLC_playlist/Josue_Yrion/
+
+mkdir Verano_Azul
+mv /home/$USER/Descargas/No_nos_moveran-Verano_Azul.mp3 /home/$USER/VLC_playlist/Verano_Azul/
+
+mkdir Sin_copyright
+mv /home/$USER/Descargas/You_reposted_in_the_wrong_neighborhood-Sin_copyright.mp3 /home/$USER/VLC_playlist/Sin_copyright/
+
+mkdir Salvador
+mv /home/$USER/Descargas/La_virgen_de_los_dolores-Salvador.mp3 /home/$USER/VLC_playlist/Salvador/
+
+mkdir Will_Smith
+mv /home/$USER/Descargas/El_principe_de_bel_air-Will_Smith.mp3 /home/$USER/VLC_playlist/Will_Smith/
+
+mkdir Cola_cao
+mv /home/$USER/Descargas/Cola_cao-Cola-cao.mp3 /home/$USER/VLC_playlist/Cola_cao/
+
+mkdir Ruso
+mv /home/$USER/Descargas/Comvitas-Ruso.mp3 /home/$USER/VLC_playlist/Ruso/
+
+mkdir RockStar
+mv /home/$USER/Descargas/GTA_San_Andreas-RockStar.mp3 /home/$USER/VLC_playlist/RockStar/
+
+mkdir Kiubbah
+mv /home/$USER/Descargas/arabe-Kiubbah_malon_many_malon_jose_victoria.mp3 /home/$USER/VLC_playlist/Kiubbah/
+
+mkdir Cristiana
+mv /home/$USER/Descargas/si-tuvieras-fe-como-un-granito-de-mostaza.mp3 /home/$USER/VLC_playlist/Cristiana/
+
+mkdir Anonimo
+mv /home/$USER/Descargas/con-un-porrito-en-la-mano.mp3 /home/$USER/VLC_playlist/Anonimo/
+
+mkdir Daddy_Yankee
+mv /home/$USER/Descargas/Soltera-Daddy_Yankee_Bad_Bunny_Lunay.mp3 /home/$USER/VLC_playlist/Daddy_Yankee/
+
+mkdir LOL
+mv /home/$USER/Descargas/Ignite_finals_remix-LOL.mp4 /home/$USER/VLC_playlist/LOL/
+mv /home/$USER/Descargas/Rise_remix-LOL.mp3 /home/$USER/VLC_playlist/LOL/
+mv /home/$USER/Descargas/Phoenix-LOL.mp3 /home/$USER/VLC_playlist/LOL/
+mv /home/$USER/Descargas/Giants-LOL.mp4 /home/$USER/VLC_playlist/LOL/
+
+mkdir Dayvi
+mv /home/$USER/Descargas/Baila_conmigo-Dayvi.mp3 /home/$USER/VLC_playlist/Dayvi/
+
+mkdir Jojos
+mv /home/$USER/Descargas/Bizarre_adventure_golden_wind_ost_il_vento_doro_golden_wind-Jojos.mp3 /home/$USER/VLC_playlist/Jojos/
+
+mkdir PP
+mv /home/$USER/Descargas/PP-PP.mp3 /home/$USER/VLC_playlist/PP/
+
+mkdir Sunstroke
+mv /home/$USER/Descargas/Run_away-Sunstroke.mp3 /home/$USER/VLC_playlist/Sunstroke/
+
+pause
