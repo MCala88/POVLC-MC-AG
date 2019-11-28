85fe28894a1eeea94b93c99066c9e337b569d3c6
Miquel Calafell <56043308+MCala88@users.noreply.github.com>
2019-11-28 17:00:19 +0100

    Delete checkso.py

--- checkso.py
+++
@@ -1,7 +0,0 @@
-import os
-import subprocess
-from subprocess import Popen
-from checkLogical import listcancion
-vlcpath = "D:/Todo/VideoLAN/VLC/vlc.exe"
-randomlist = " ".join(listcancion)
-v = subprocess.Popen([vlcpath, [randomlist]])
